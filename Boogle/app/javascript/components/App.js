f3bff421ef37086db812e89b4f9e925e1c6ca6b7
Global Digital <nabinwell@gmail.com>
2020-03-30 13:22:10 +0545

    Boogle grid with random function

--- Boogle/app/javascript/components/App.js
+++ Boogle/app/javascript/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import Cells from './Cells'
+
+const container = {
+ 
+ 
+display: 'flex',  justifyContent:'center', alignItems:'center', 
+ width:'100%'
+ 
+
+ 
+};
+
 
 class App extends React.Component{
 
+ 
+	
+
 	render(){
 
-      return(<div>hello game component</div>)
+      return(<div style={container}><Cells/></div>)
 
 		}
 
 
 }
 export default App
+
---
+++ Boogle/app/javascript/components/Cells.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const divStyle = {
+ 
+ width:'200px',
+
+
+};
+
+const divStyle1 = {
+ background:'#eee',
+ border:'1px solid #000',
+display: 'flex',  justifyContent:'center', alignItems:'center', height: '5vh',
+ 
+ width:'20%',
+ float:'left',
+
+ 
+};
+
+
+
+
+
+
+const Cells = (props) => {
+      let   alpha=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Z']
+       let a1=alpha.sort(() => 0.5 - Math.random()).slice(0, 4)
+       let a2=alpha.sort(() => 0.5 - Math.random()).slice(0, 4)
+       let a3=alpha.sort(() => 0.5 - Math.random()).slice(0, 4)
+       let a4=alpha.sort(() => 0.5 - Math.random()).slice(0, 4)
+      
+       
+
+    let mtrx=[a1,a2,a3,a4]
+
+    let cells = []
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++) {
+            cells.push(<div class="grid-item" style={divStyle1} key={i+''+j}>{mtrx[i][j] }</div>)
+        }
+    }
+    return (
+        <div class="grid-container" style={divStyle} >
+            {cells}
+        </div>
+    )
+}
+
+export default Cells
